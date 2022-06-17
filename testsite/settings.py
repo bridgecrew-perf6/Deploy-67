51b4f250fef82eb651c39b5ea401a3db9fb63c09
cipher <fynhacker@gmail.com>
2022-06-17 14:07:26 +0300

    small change

--- testsite/settings.py
+++ testsite/settings.py
@@ -10,61 +10,35 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-# from pathlib import Path
-# import environ
-# import os
-
-# env = environ.Env(
-#     # set casting, default value
-#     DEBUG=(bool, False)
-# )
-
-# # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-
-# # Take environment variables from .env file
-# environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-
-# # False if not in os.environ because of casting above
-
-
-# # Raises Django's ImproperlyConfigured
-# # exception if SECRET_KEY not in os.environ
-# SECRET_KEY = env('SECRET_KEY')
-
-# # Quick-start development settings - unsuitable for production
-# # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# # SECURITY WARNING: keep the secret key used in production secret!
-
-
-# # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = env('DEBUG')
-
 from pathlib import Path
 import environ
-import os 
+import os
 
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
-# Take environment variables from .env file
-environ.Env.read_env()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env()
+
+# False if not in os.environ because of casting above
+
+
+# Raises Django's ImproperlyConfigured
+# exception if SECRET_KEY not in os.environ
+SECRET_KEY = env('SECRET_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
-
 
 ALLOWED_HOSTS = ['*']
 
